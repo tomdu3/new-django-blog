62d35a2c446afadf02725b29c3a54bfc6c925114
Tomislav Dukez <tomdu3@ymail.com>
2024-06-20 20:38:17 +0200

    add comment model

--- blog/models.py
+++ blog/models.py
@@ -19,3 +19,17 @@ class Post(models.Model):
     def __str__(self):
 
         return f'{self.title} | written by{self.author}'
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment on post: {self.post.title} by {self.author}"
