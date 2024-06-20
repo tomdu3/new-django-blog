787b660843b2858c843e1adc8b9d9e74ec6e6d3f
Tomislav Dukez <tomdu3@ymail.com>
2024-06-20 20:41:44 +0200

    update comment model

--- blog/models.py
+++ blog/models.py
@@ -32,4 +32,4 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment on post: {self.post.title} by {self.author}"
+        return f"Comment {self.body} by {self.author}"
