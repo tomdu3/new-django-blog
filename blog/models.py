b6ee950e2491ec4a77f12ad7f8f25cd6992815e6
Tomislav Dukez <tomdu3@ymail.com>
2024-06-20 20:58:49 +0200

    update post model exerpt and updated_on

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,10 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
 
     class Meta:
         ordering = ["-created_on"]  # descending order
@@ -23,7 +27,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments_author")
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
