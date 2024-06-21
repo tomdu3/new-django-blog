1f16c12248e1fe879705e871d04bc87f4b39c4ee
Tomislav Dukez <tomdu3@ymail.com>
2024-06-21 16:37:25 +0200

    added post_detail view

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.Postlist.as_view(), name="home"),
+    path('post/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -7,3 +7,29 @@ class Postlist(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    query_set = Post.objects.filter(status=1)
+    template_name = "blog/post_detail.html"
+
+    def get_object(self):
+        return get_object_or_404(Post.objects.filter(status=1), slug=self.kwargs['slug'])
+
+## original function view code
+# def post_detail(request, slug):
+#     """
+#     Display an individual :model:`blog.Post`.
+
+#     **Context**
+
+#     ``post``
+#         An instance of :model:`blog.Post`.
+
+#     **Template:**
+
+#     :template:`blog/post_detail.html`
+#     """
+#     queryset = Post.objects.filter(status=1)
+#     post = get_object_or_404(queryset, slug=slug)
+#     return render(request, "blog/post_detail.html", {"post": post},)
