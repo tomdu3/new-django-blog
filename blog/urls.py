aded4d2f9f0ec1f4a0408a4f31092453479f2ab0
Tomislav Dukez <tomdu3@ymail.com>
2024-06-21 10:25:58 +0200

    update postlist view url name

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.Postlist.as_view(), name="post-list"),
+    path("", views.Postlist.as_view(), name="home"),
 ]
