568a967b4d974c565073fc13cf30c2336aed6ff2
Tomislav Dukez <tomdu3@ymail.com>
2024-06-21 22:04:25 +0200

    fix refactor to aboutmeview class

--- about/views.py
+++ about/views.py
@@ -2,24 +2,24 @@ from django.shortcuts import render
 from .models import About
 from django.views.generic import DetailView
 
-# class AboutMeView(DetailView):
-#     model = About
-#     template_name = "about/about.html"
-#     context_object_name = "about"
+class AboutMeView(DetailView):
+    model = About
+    template_name = "about/about.html"
+    context_object_name = "about"
 
-#     def get_object(self):
-#         # Return the latest About object based on the updated_on field
-#         return About.objects.all().order_by('-updated_on').first()
+    def get_object(self):
+        # Return the latest About object based on the updated_on field
+        return About.objects.all().order_by('-updated_on').first()
 
-# original function view code
-def about_me(request):
-    """
-    Renders the About page
-    """
-    about = About.objects.all().order_by('-updated_on').first()
+# # original function view code
+# def about_me(request):
+#     """
+#     Renders the About page
+#     """
+#     about = About.objects.all().order_by('-updated_on').first()
 
-    return render(
-        request,
-        "about/about.html",
-        {"about": about},
-    )
+#     return render(
+#         request,
+#         "about/about.html",
+#         {"about": about},
+#     )
