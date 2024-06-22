9ec838f5becb9d133f4a0550dfdabce817314644
Tomislav Dukez <tomdu3@ymail.com>
2024-06-22 12:45:28 +0200

    add TODO: change function view to a class

--- blog/views.py
+++ blog/views.py
@@ -109,6 +109,7 @@ def comment_delete(request, slug, comment_id):
     """
     view to delete comment
     """
+    # TODO: Change function view into a class based view
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comment = get_object_or_404(Comment, pk=comment_id)
