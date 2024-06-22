a6ef4ff980e9a87105c0e1ead1378f553f69d132
Tomislav Dukez <tomdu3@ymail.com>
2024-06-22 11:11:19 +0200

    add collaboration view

--- about/views.py
+++ about/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import About
 from django.views.generic import DetailView
-
+from .forms import CollaborateRequestForm
+from django.contrib import messages
 class AboutMeView(DetailView):
     model = About
     template_name = "about/about.html"
@@ -10,6 +11,26 @@ class AboutMeView(DetailView):
     def get_object(self):
         # Return the latest About object based on the updated_on field
         return About.objects.all().order_by('-updated_on').first()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['collaborate_form'] = CollaborateRequestForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = CollaborateRequestForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(
+                request, messages.SUCCESS, 'Thank you for your collaboration request!',
+            )
+            # Redirect to the same about page after saving the form
+            return redirect('about')
+        else:
+            context = self.get_context_data()
+            context['collaborate_form'] = form
+            return self.render_to_response(context)
+
 
 # # original function view code
 # def about_me(request):
